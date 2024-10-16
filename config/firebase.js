8393a69b75d3334fde46d45df4e8863c5bd89120
DonSam77 <samuel.campos77@outlook.com>
2024-10-16 18:01:59 -0300

    guardando cambios antes de limpiar historial

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./nodejs-1cc38-firebase-adminsdk-yno4r-6f71a838ae.json'); //archivo de credenciales
+//const serviceAccount = require('./nodejs-1cc38-firebase-adminsdk-yno4r-6f71a838ae.json'); //archivo de credenciales
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://nodejs-1cc38.firebaseio.com'  //URL de la base de datos Firebase!
+    //databaseURL: 'https://nodejs-1cc38.firebaseio.com'  //URL de la base de datos Firebase!
 });
 
 const db = admin.firestore();
